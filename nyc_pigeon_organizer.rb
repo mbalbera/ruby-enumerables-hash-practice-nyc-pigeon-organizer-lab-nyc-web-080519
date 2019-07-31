2d3aa638420138c5af94a822654f74a02fcb8616
Mason Balbera <mhbalbera@gmail.com>
2019-07-31 13:30:22 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  answer = Hash.new()
+  data.each do |k, v|
+     v.each do |attr_v, names|
+       names.each do |n|
+         if !answer[n]
+           answer[n] = Hash.new()
+         end
+         if !answer[n][k]
+            answer[n][k] = Array.new()
+        end
+        answer[n][k] << attr_v.to_s
+      end 
+    end
+  end
+  answer
 end
